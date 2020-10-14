ea7afbd87f8b2ec1d9a9ba81377fa76514919d61
karamalesa tv <karamalesa@gmail.com>
2020-10-14 13:06:39 +0300

    new /docs 13:06

---
+++ docs/precache-manifest.34188b1574d83ac8de8db78595624c58.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d13c16219ef794990f622d1ab8c3b02",
+    "url": "/bestapp/index.html"
+  },
+  {
+    "revision": "387cec68f46a911d8b3e",
+    "url": "/bestapp/static/css/2.95908701.chunk.css"
+  },
+  {
+    "revision": "7a3a0e7bfac3bcabfb3e",
+    "url": "/bestapp/static/css/main.3138266b.chunk.css"
+  },
+  {
+    "revision": "387cec68f46a911d8b3e",
+    "url": "/bestapp/static/js/2.cf341e56.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bestapp/static/js/2.cf341e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3a0e7bfac3bcabfb3e",
+    "url": "/bestapp/static/js/main.0cef2f59.chunk.js"
+  },
+  {
+    "revision": "638796c32d23a309bcbb",
+    "url": "/bestapp/static/js/runtime-main.93ee5616.js"
+  },
+  {
+    "revision": "71566586223ef694ac001f623fe272b6",
+    "url": "/bestapp/static/media/2-sm.71566586.png"
+  },
+  {
+    "revision": "bd1ee0cb14aa910eea5122d9ca08c3ea",
+    "url": "/bestapp/static/media/facts-1.bd1ee0cb.jpg"
+  },
+  {
+    "revision": "a020ec5b4d06b3eacb5eb66d536b99da",
+    "url": "/bestapp/static/media/facts-2.a020ec5b.jpg"
+  },
+  {
+    "revision": "bc0ef521644f8572ec8a5b5b06ee5963",
+    "url": "/bestapp/static/media/facts-3.bc0ef521.jpg"
+  },
+  {
+    "revision": "257c09794128d582589d06421b0a2cca",
+    "url": "/bestapp/static/media/icon_map.257c0979.svg"
+  },
+  {
+    "revision": "444ab4f6335f11975e4300ef1038d958",
+    "url": "/bestapp/static/media/img-1.444ab4f6.png"
+  },
+  {
+    "revision": "43aa24dbbdd779778df66f0540da976d",
+    "url": "/bestapp/static/media/img-2.43aa24db.png"
+  },
+  {
+    "revision": "2923ce8e802c16bd73239f4b20a278c3",
+    "url": "/bestapp/static/media/img-3.2923ce8e.png"
+  },
+  {
+    "revision": "51028ac6df09dc43ed2fdaa949615983",
+    "url": "/bestapp/static/media/img-4.51028ac6.png"
+  },
+  {
+    "revision": "1ddccca07628cfb2053d5aa0bcffc5ba",
+    "url": "/bestapp/static/media/img-5.1ddccca0.png"
+  },
+  {
+    "revision": "1ebd89da1d775cfa8d1e6456d9999af0",
+    "url": "/bestapp/static/media/img-6.1ebd89da.png"
+  },
+  {
+    "revision": "97bfa4dac547c1f4cfd3923754c56881",
+    "url": "/bestapp/static/media/photo.97bfa4da.webp"
+  }
+]);
